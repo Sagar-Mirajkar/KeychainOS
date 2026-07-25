0001a1e7f48bc219b205cbe9e95aa90fd2cf1ada
Sagar-Mirajkar <101185106+Sagar-Mirajkar@users.noreply.github.com>
2026-07-25 15:25:25 +0530

    Add OTA Python module test

---
+++ ota/update_test.py
@@ -0,0 +1,9 @@
+"""Harmless KeychainOS OTA Python module test."""
+
+VERSION = "0.2"
+
+
+def run():
+    print("Downloaded Python module is working")
+    print("OTA module version:", VERSION)
+    return True
